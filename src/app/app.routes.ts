de14e61da06153b050c2a8aff8e7b324d6a0f0bf
dvfer <diego.pablo23@gmail.com>
2024-02-01 15:50:11 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { RootViewComponent } from './root-view/root-view.component';
+import { AboutUsViewComponent } from './about-us-view/about-us-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component : RootViewComponent,
+    },
+    {
+        path: 'nosotros',
+        component: AboutUsViewComponent,
+    }
+
+
+
+];
---
+++ src/app/root-view/root-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './root-view.component.html',
+  styleUrl: './root-view.component.css'
+})
+export class RootViewComponent {
+
+}
